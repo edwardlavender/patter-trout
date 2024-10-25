7e3a39d17a633dfe9fa82fcb8597d06e8e8876b9
edwardlavender <edward.lavender@eawag.ch>
2024-10-25 10:56:42 +0200

    Cleanup workflow and permit three models

--- src/patter-workflow.R
+++ src/patter-workflow.R
@@ -1,3 +1,5 @@
+#' Patter workflow
+
 # Run the particle algorithms for a selected individual
 # This workflow assumes:
 # * Select packages and functions are available & loaded
@@ -7,14 +9,16 @@
 patter_workflow <- function(id, 
                             map = NULL, map_len,
                             timelines, paths, moorings, detections, metadata, 
-                            parameters,
+                            parameters, model_move = c("low", "medium", "high"),
                             interactive = TRUE) {
   
-  # id   <- 1
+  # id          <- 2L
   # interactive <- TRUE
+  model_move    <- match.arg(model_move)
+
   
   ###########################
-  #### Prepare algorithms
+  #### Prepare data
   
   #### Define individual datasets
   sim_id <- NULL
@@ -39,40 +43,103 @@ patter_workflow <- function(id,
     # * (optional) TO DO
   }
   
+  
+  ###########################
   #### Define movement model 
-  
-  # Parameters
-  mobility <- parameters$model_move$mobility
-  sshape   <- parameters$model_move$step$shape
-  sscale   <- parameters$model_move$step$scale
-  amean    <- parameters$model_move$angle$mean
-  asd      <- parameters$model_move$angle$sd
-  
-  # Random walk
-  # > This is associated with convergence issues
+
+  #### Random walk
+  # This is a simple generalisable model
+  # But it is associated with convergence issues 
   # state      <- "StateXY"
+  # sspec    <- "gamma"
+  mobility <- parameters$model_move$mobility
+  # sshape   <- parameters$model_move$step$shape
+  # sscale   <- parameters$model_move$step$scale
+  # amean    <- parameters$model_move$angle$mean
+  # asd      <- parameters$model_move$angle$sd
   # model_move <- move_xy(mobility = mobility, 
   #                       dbn_length = glue("truncated(Gamma({sshape}, {sscale}), lower = 0.0, upper = {mobility})"), 
   #                       dbn_angle = glue("truncated(Normal(0, 1), lower = -pi, upper = pi)"))
+
+  #### Correlated random walk (generalised)
+  # A CRW has better convergence properties
+  # However, it is difficult to capture the step length distribution adequately across all individuals
+  # The turning angle distribution is acceptible:
+  # * This is inflated slightly to limit performance cost
+  # * Overly high concentration leads to slow algorithm runs
+  # ... as many trials are required to avoid jumps onto land
+  # * But a sd of <= 0.3 appears to be required for convergence
+  # state      <- "StateXYD"
+  # sspec      <- "gamma"
+  # sshape     <- sshape
+  # sscale     <- 60
+  # amean      <- 0.0
+  # asd        <- 0.3
+  # state      <- "StateXYD"
+  # model_move <- move_xyd(mobility = mobility, 
+  #                        dbn_length = glue("truncated(Gamma({sshape}, {sscale}), lower = 0.0, upper = {mobility})"), 
+  #                        dbn_angle_delta = glue("Normal({amean}, {asd})"))
+
+  #### Correlated random walk (low activity)
+  # TO DO
+  if (model_move == "low") {
+    state      <- "StateXYD"
+    sspec      <- "gamma"
+    sshape     <- 3
+    sscale     <- 6
+    amean      <- 0.0
+    asd        <- 0.3
+    model_move <- move_xyd(mobility = mobility, 
+                           dbn_length = glue("truncated(Normal({sshape}, {sscale}), lower = 0.0, upper = {mobility})"), 
+                           dbn_angle_delta = glue("Normal({amean}, {asd})"))
+    
+    
+    curve(dtrunc(x, spec = "gamma", a = 0, b = mobility, shape = sshape, scale = sscale), 
+          from = 0, to = mobility, n = 1e3L,
+          xlab = "Step length (m)", ylab = "Density")
+    abline(v = 12.7)
+  }
   
-  # Correlated random walk
-  # > This has better convergence properties
-  # > The turning angle concentration is turned to limit performance cost
-  # > Overly high concentration leads to slow algorithm runs
-  # > ... as many trials are required to avoid jumps onto land
-  state      <- "StateXYD"
-  model_move <- move_xyd(mobility = mobility, 
-                         dbn_length = glue("truncated(Gamma({sshape}, {60}), lower = 0.0, upper = {mobility})"), 
-                         dbn_angle_delta = glue("Normal({0.0}, {0.4})"))
+  #### Correlated random walk (medium activity)
+  if (model_move == "medium") {
+    state      <- "StateXYD"
+    sspec       <- "norm"
+    sshape     <- 63.5
+    sscale     <- 5
+    amean      <- 0.0
+    asd        <- 0.3
+    model_move <- move_xyd(mobility = mobility, 
+                           dbn_length = glue("truncated(Normal({sshape}, {sscale}), lower = 0.0, upper = {mobility})"), 
+                           dbn_angle_delta = glue("Normal({amean}, {asd})"))
+  }
+
+  #### Correlated random walk (high activity)
+  if (model_move == "high") {
+    state      <- "StateXYD"
+    sspec      <- "gamma"
+    sshape     <- 20
+    sscale     <- 10
+    amean      <- 0.0
+    asd        <- 0.3
+    model_move <- move_xyd(mobility = mobility, 
+                           dbn_length = glue("truncated(Gamma({sshape}, {sscale}), lower = 0.0, upper = {mobility})"), 
+                           dbn_angle_delta = glue("Normal({amean}, {asd})"))
+  }
 
   #### Visualise movement model 
   if (interactive) {
     
     # Visualise model components 
     pp <- par(mfrow = c(1, 2))
-    curve(dtrunc(x, spec = "gamma", a = 0, b = mobility, shape = sshape, scale = sscale), 
-          from = 0, to = mobility, n = 1e3L,
-          xlab = "Step length (m)", ylab = "Density")
+    if (sspec == "gamma") {
+      curve(dtrunc(x, spec = "gamma", a = 0, b = mobility, shape = sshape, scale = sscale), 
+            from = 0, to = mobility, n = 1e3L,
+            xlab = "Step length (m)", ylab = "Density")
+    } else if (sspec == "norm") {
+      curve(dtrunc(x, spec = "norm", a = 0, b = mobility, mean = sshape, sd = sscale), 
+            from = 0, to = mobility, n = 1e3L,
+            xlab = "Step length (m)", ylab = "Density")
+    }
     abline(v = max(dist), col = "red")
     curve(dnorm(x, 0, 0.25),
           from = -pi - 0.1, to = pi + 0.1, n = 1e3L,
@@ -104,18 +171,23 @@ patter_workflow <- function(id,
     
   }
   
+  
+  ###########################
   #### Define observation models
-  # Assemble acoustics (0, 1)
+  
+  #### Assemble acoustics (0, 1)
   moorings[, receiver_gamma := parameters$model_obs$receiver_gamma]
   acoustics   <- assemble_acoustics(.timeline   = timeline, 
                                     .detections = dets, 
                                     .moorings   = moorings)
-  # Assemble containers
+  
+  #### Assemble containers
   containers  <- assemble_acoustics_containers(.timeline  = timeline, 
                                                .acoustics = acoustics, 
                                                .mobility  = mobility, 
                                                .threshold = map_len)
-  # Define yobs (forward & backward)
+  
+  #### Define yobs (forward & backward)
   yobs_fwd <- list(ModelObsAcousticLogisTrunc = copy(acoustics),
                    ModelObsAcousticContainer  = copy(containers$forward))
   yobs_bwd <- list(ModelObsAcousticLogisTrunc = copy(acoustics),
@@ -130,16 +202,17 @@ patter_workflow <- function(id,
   #### Algorithm runs 
   
   #### Define filter arguments 
+  # Tune .n_move, .n_particle & .n_record for improved speed during filtering/smoothing
   args <- list(.timeline = timeline, 
                .state = state, 
                .xinit = xinit_fwd, 
                .yobs = yobs_fwd,
                .model_move = model_move, 
+               .n_move = 10000L,
                .n_particle = 2.5e4L,
                .n_record = 500L,
-               .n_iter = 2L,
-               .direction = "forward"
-  )
+               .n_iter = 1L,
+               .direction = "forward")
   
   #### Run forward filter 
   # Setting initial observations is slow (~1 min)
@@ -150,18 +223,19 @@ patter_workflow <- function(id,
   t2             <- Sys.time()
   convergence_dt <- data.table(sim_id      = id, 
                                direction   = "forward", 
+                               model_move  = model_move,
                                np          = args$.n_particle, 
                                nt          = length(timeline),
                                time        = as.numeric(difftime(t2, t1, units = "mins")),
                                convergence = fwd$convergence, 
                                trials      = fwd$trials)
   qs::qsave(convergence_dt, 
-            here_output("convergence", glue("convergence-fwd-{id}.qs")))
+            here_output_sim("convergence", model_move, glue("convergence-fwd-{id}.qs")))
   if (!fwd$convergence) {
     return(NULL)
   }
-  # qs::qsave(fwd, here_output("tmp", "tmp.qs"))
-  # file.size(here_output("tmp", "tmp.qs")) * 1e-9 # 4 GB
+  # qs::qsave(fwd, here_output_sim("tmp", "tmp.qs"))
+  # file.size(here_output_sim("tmp", "tmp.qs")) * 1e-9 # 4 GB
   
   #### Run backward filter
   args$.xinit     <- xinit_bwd
@@ -173,25 +247,27 @@ patter_workflow <- function(id,
   t2              <- Sys.time()
   convergence_dt  <- data.table(sim_id      = id, 
                                 direction   = "backward", 
+                                model_move  = model_move,
                                 np          = args$.n_particle, 
                                 nt          = length(timeline),
                                 time        = as.numeric(difftime(t2, t1, units = "mins")),
                                 convergence = bwd$convergence, 
                                 trials      = bwd$trials)
   qs::qsave(convergence_dt, 
-            here_output("convergence", glue("convergence-bwd-{id}.qs")))
+            here_output_sim("convergence", model_move, glue("convergence-bwd-{id}.qs")))
   if (!bwd$convergence) {
     return(NULL)
   }
   
   #### Run smoother 
-  # ETA without vmap: XXX mins
-  # ETA with vmap:    XXX mins
+  # (TO DO) ETA without vmap: XXX mins
+  # (TO DO) ETA with vmap:    XXX mins
   t1   <- Sys.time()
   set_seed()
   smo  <- pf_smoother_two_filter()
   t2   <- Sys.time()
   convergence_dt  <- data.table(sim_id      = id, 
+                                model_move  = model_move,
                                 direction   = "smoothing", 
                                 np          = args$.n_particle, 
                                 nt          = length(timeline),
@@ -199,9 +275,9 @@ patter_workflow <- function(id,
                                 convergence = NA_integer_, 
                                 trials      = NA_integer_)
   qs::qsave(convergence_dt, 
-            here_output("convergence", glue("convergence-smo-{id}.qs")))
-  qs::qsave(smo, here_output("particles", glue("smo-{id}.qs")))
-  # file_size(here_output("particles", glue("smo-{id}.qs")))
+            here_output_sim("convergence", model_move, glue("convergence-smo-{id}.qs")))
+  qs::qsave(smo, here_output_sim("particles", model_move, glue("smo-{id}.qs")))
+  # file_size(here_output_sim("particles", model_move, glue("smo-{id}.qs"))) # 757 MB
   
   NULL
   
