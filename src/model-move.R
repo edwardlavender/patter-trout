c624d69875a1287e07b9711780b3280e3590e0c4
edwardlavender <edward.lavender@eawag.ch>
2024-10-25 10:56:49 +0200

    Cleanup

--- src/model-move.R
+++ src/model-move.R
@@ -1,3 +1,4 @@
+#' Two-dimensional CRW
 move_xyd <- function(mobility = "750.0",
                      dbn_length = "truncated(Gamma(1.0, 750.0), upper = 750.0)",
                      dbn_angle_delta = "Normal(0, 0.5)") {
